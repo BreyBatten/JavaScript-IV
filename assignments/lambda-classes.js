aa8a3afcf2afad3483d4eb668136c6b8a685d013
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-04-04 22:16:07 -0400

    updated file with more classes
    I have added classes for each type of person and the methods for each. I have started to create objects and test the output.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,64 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.specialty = instructorAttrs.specialty;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}.`
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects; 
+    }
+    listsSubjects() {
+        this.favSubjects.forEach((value) => console.log(value));
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttrs) {
+        super(pmAttrs);
+        this.gradClassName = pmAttrs.gradClassName;
+        this.favInstructor = pmAttrs.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standup time!`
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}. `
+    }
+}
+
+
+const brey = new Student({
+    name: 'Brey',
+    location: 'Virginia',
+    age: 25,
+    gender: 'male',
+    previousBackground: 'Zipline Guide',
+    className: 'WEBPT5',
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+});
+console.log(brey);
+console.log(brey.speak());
+console.log(brey.listsSubjects(brey.favSubjects));
