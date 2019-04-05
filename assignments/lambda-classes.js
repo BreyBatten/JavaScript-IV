37a18cbf563037ea30c7cdc511b25ff1092b2d86
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-04-04 20:49:12 -0400

    added Person class to lambda-classes.js
    I added the Person class to the file and gave Person the props of name, age, location, gender. I also added a speak method to be returned.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+// Person class
+class Person {
+    constructor(base) {
+        this.name = base.name;
+        this.age = base.age;
+        this.location = base.location;
+        this.gender = base.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
